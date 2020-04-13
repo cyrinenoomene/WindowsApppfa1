a41316bb5d23ad5506cea3e850bdaf0617a5de3c
cyrin <cyrin@DESKTOP-9CS4D7I>
2020-04-13 17:28:57 +0200

    Login

---
+++ WindowsApppfa1/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Data.SqlClient
+
+
+Public Class Form1
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+	End Sub
+
+	'login****'
+
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Dim con As New SqlConnection
+		Dim cmd As New SqlCommand
+		Dim Password As String
+		Dim Password2 As String
+		Dim userName As String
+		Try
+
+			con.ConnectionString = "server=DESKTOP-9CS4D7I;Database=Authentification;Integrated Security=True "
+			con.Open()
+			cmd.Connection = con
+			cmd.CommandText = " SELECT  UserName, Password FROM dbo.LOGIN WHERE UserName='user' and Password='123'"
+			Dim rd As SqlDataReader = cmd.ExecuteReader()
+			If rd.HasRows Then
+				While rd.Read()
+					Password = rd("Password").ToString()
+					userName = rd("UserName").ToString()
+					Password2 = txtPassword.Text()
+
+					If Password = Password2 And userName = TxtUsername.Text Then
+
+						MessageBox.Show("Logged in successfully as " & userName, "", MessageBoxButtons.OK, MessageBoxIcon.Information
+									)
+						frmMain.Show()
+						Me.Hide()
+						TxtPassword.Text = ""
+						TxtUsername.Text = ""
+
+					End If
+
+
+				End While
+			Else
+				MessageBox.Show("Username and Password do not match..", "Authentication Failure", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+
+				TxtPassword.Text = ""
+				TxtUsername.Text = ""
+			End If
+		Catch ex As Exception
+			MessageBox.Show("Error while connecting to SQL Server." & ex.Message)
+		Finally
+			con.Close()
+
+		End Try
+
+
+	End Sub
+
+	Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+		TxtUsername.Text = ""
+		TxtPassword.Text = ""
+		TxtUsername.Focus()
+
+	End Sub
+
+	Private Sub TxtUsername_TextChanged(sender As Object, e As EventArgs) Handles TxtUsername.TextChanged
+
+	End Sub
+End Class
---
+++ WindowsApppfa1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Vérifiez les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("WindowsApppfa1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WindowsApppfa1")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("c83ac814-6215-45cf-bd3c-b69b1d5ba9c7")>
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
